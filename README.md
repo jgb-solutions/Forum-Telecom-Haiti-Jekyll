58c8fcebe1b14736b5d91c80dad5619797294cc2
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2017-03-31 23:21:29 -0500

    Cleaned some things up

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Forum-Telecom-Haiti-Jekyll
-Forum Telecom Haiti site built on Jekyll
+# Forum Telecom Haiti
+
+This repository hold the source code for the **Forum Telecom Haiti** website.
+It's built on **Jekyll** and hosted on Github Pages.
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,49 @@
-function collapseNavbar(){$(".navbar").offset().top>50?$(".navbar-fixed-top").addClass("top-nav-collapse"):$(".navbar-fixed-top").removeClass("top-nav-collapse")}$(window).scroll(collapseNavbar),$(document).ready(collapseNavbar),$(function(){$("a.page-scroll").bind("click",function(a){var o=$(this);$("html, body").stop().animate({scrollTop:$(o.attr("href")).offset().top},1500,"easeInOutExpo"),a.preventDefault()})}),$(".navbar-collapse ul li a").click(function(){"dropdown-toggle active"!=$(this).attr("class")&&"dropdown-toggle"!=$(this).attr("class")&&$(".navbar-toggle:visible").click()}),$(function(){FlipClock.Lang.Custom={days:"Jours",hours:"Heures",minutes:"Minutes",seconds:"Secondes"};var a={clockFace:"DailyCounter",countdown:!0,language:"Custom"},o=1495033320-(new Date).getTime()/1e3;o=Math.max(1,o),$(".clock-builder-output-2017").FlipClock(o,a);var e=1463497200-(new Date).getTime()/1e3;e=Math.max(1,e),$(".clock-builder-output-2016").FlipClock(e,a)});
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+	if ($(".navbar").offset().top > 50) {
+		$(".navbar-fixed-top").addClass("top-nav-collapse");
+	} else {
+		$(".navbar-fixed-top").removeClass("top-nav-collapse");
+	}
+}
+
+$(window).scroll(collapseNavbar);
+
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+	$('.navbar-toggle:visible').click();
+  }
+});
+
+
+/* Countdown */
+$(function() {
+	FlipClock.Lang.Custom = { days:'Jours', hours:'Heures', minutes:'Minutes', seconds:'Secondes' };
+	var opts = {
+		clockFace: 'DailyCounter',
+		countdown: true,
+		language: 'Custom'
+	};
+	var countdown2017 = 1495033320 - ((new Date().getTime())/1000); // from: 05/17/2016 09:00 am -0600
+	countdown2017 = Math.max(1, countdown2017);
+	$('.clock-builder-output-2017').FlipClock(countdown2017, opts);
+
+	var countdown2016 = 1463497200 - ((new Date().getTime())/1000); // from: 05/17/2016 09:00 am -0600
+	countdown2016 = Math.max(1, countdown2016);
+	$('.clock-builder-output-2016').FlipClock(countdown2016, opts);
+});
+//# sourceMappingURL=site.js.map
