a57b44e2e4cdf3a8eb764f4b122a04f823e90549
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2017-04-03 12:40:19 -0500

    Quick Commit

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,38 @@
-function collapseNavbar(){$(".navbar").offset().top>50?$(".navbar-fixed-top").addClass("top-nav-collapse"):$(".navbar-fixed-top").removeClass("top-nav-collapse")}$(window).scroll(collapseNavbar),$(document).ready(collapseNavbar),$(function(){$("a.page-scroll").bind("click",function(a){var o=$(this);$("html, body").stop().animate({scrollTop:$(o.attr("href")).offset().top},1500,"easeInOutExpo"),a.preventDefault()})}),$(".navbar-collapse ul li a").click(function(){"dropdown-toggle active"!=$(this).attr("class")&&"dropdown-toggle"!=$(this).attr("class")&&$(".navbar-toggle:visible").click()}),$(function(){$("#counter").countdownCube({target:new Date("May 17, 2017 10:00:00"),cubeSize:100})});
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+	if ($(".navbar").offset().top > 50) {
+		$(".navbar-fixed-top").addClass("top-nav-collapse");
+	} else {
+		$(".navbar-fixed-top").removeClass("top-nav-collapse");
+	}
+}
+
+$(window).scroll(collapseNavbar);
+
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  	if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+		$('.navbar-toggle:visible').click();
+  	}
+});
+
+$(function() {
+	$('#counter').countdownCube({
+		target: new Date( 'May 17, 2017 10:00:00' ),
+		cubeSize: 100
+	});
+});
+//# sourceMappingURL=site.js.map
