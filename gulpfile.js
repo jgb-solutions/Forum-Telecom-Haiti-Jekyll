4a2c32e30a7ea0544cfae81156f912629be8c1ca
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2017-05-09 15:48:37 -0500

    Quick Commit

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,15 @@ elixir(function(mix) {
 		.scripts('site.js')
 
 		.combine([
-			'node_modules/countdownCube-master/countdowncube.min.css',
+			// 'node_modules/countdownCube-master/countdowncube.min.css',
+			'node_modules/tutorialzine/assets/countdown/jquery.countdown.css',
 		], 'assets/css/vendor.css')
 
 		.combine([
 			'node_modules/jquery/dist/jquery.min.js',
 			'node_modules/bootstrap/dist/js/bootstrap.min.js',
 			'node_modules/jquery.easing/jquery.easing.min.js',
-			'node_modules/countdownCube-master/countdowncube.js'
+			// 'node_modules/countdownCube-master/countdowncube.js',
+			'node_modules/tutorialzine/assets/countdown/jquery.countdown.js'
 		], 'assets/js/vendor.js');
 });
